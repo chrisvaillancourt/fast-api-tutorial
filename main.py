cf1f33b8adffeabbc8b014b041fa6c412e54f248
Chris Vaillancourt <cvaillan22@gmail.com>
2021-01-19 16:51:49 -0800

    feat: add path parameters example

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+# `@app.get('/)`` tells FastAPI that the fun below handles requests that go to
+# the path `/` and use a `get` operation
+# It's formally referred to as a 'path operation decorator'
 @app.get('/')
 async def root():
     return {'message': 'Hello World'}
+
+
+@app.get('/items/{item_id}')
+async def read_item(item_id: int):
+    return {'item_id': item_id}
